1d0c9321267c91ce6ee282cabe712b4c8b6ec812
Conor O'Riordan <oriordanc@office.local>
2020-10-01 13:36:57 +0100

    For loop recap

---
+++ squares.py
@@ -0,0 +1,17 @@
+#for loops for lists
+
+for i in range(20):
+    print(f"{i:2} {i*i:4}")
+    
+
+print(" ")
+
+for i in range(50):
+    if i % 2 != 0:
+        print(i)
+
+
+print(range(20))
+
+#range generator
+print(list(range(20)))
