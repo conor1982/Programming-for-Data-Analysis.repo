9fcefccc3ca44701f0bf5fd31ca766330d3f2a89
Conor O'Riordan <oriordanc@office.local>
2020-10-09 11:07:32 +0100

    2dplot

--- matplotlib-examples/2dplot.py
+++ matplotlib-examples/2dplot.py
@@ -1,3 +1,11 @@
 import matplotlib.pyplot as plt
 
-l = [1,2,3,4,5]
+#line
+#plt.plot([1,2,3,4],[1,4,9,16],'b')
+#plt.ylabel('Some Numbers')
+#plt.show()
+
+#points                        #letter color . is point
+plt.plot([1,2,3,4],[1,4,9,16],'b.')
+plt.ylabel('Some Numbers')
+plt.show()
